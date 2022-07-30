d3f8c3de3beb3221a5ccc7b2f5db317342148f45
homayoonalimohammadi <homayoonalimohammadi@gmail.com>
2022-07-30 12:50:19 +0430

    Updated functions type hints

--- website/polls/problems.py
+++ website/polls/problems.py
@@ -1,42 +1,44 @@
+from __future__ import annotations
 from datetime import date, timedelta
+from typing import Any
 from polls.models import Post
 from django.db.models import Q
 
 
-def find_in_titles_1(word: str):
+def find_in_titles_1(word: str) -> list[Post]:
     posts = Post.objects.filter(
         title__icontains=word, pub_date__gte=date.today() - timedelta(days=30)
     )
-    return posts
+    return list(posts)
 
 
-def find_today_10_likes_2():
+def find_today_10_likes_2() -> list[Post]:
     today = date.today()
     posts = Post.objects.filter(number_of_likes__gt=10, pub_date=today)
-    return posts
+    return list(posts)
 
 
-def fetch_today_titles_3():
+def fetch_today_titles_3() -> list[Post]:
     today = date.today()
     qs = Post.objects.filter(pub_date=today).all()
     post_titles = [post.title for post in qs]
-    return post_titles
+    return list(post_titles)
 
 
-def fetch_comments_between_50_100_4():
+def fetch_comments_between_50_100_4() -> list[Post]:
     posts = Post.objects.filter(
         title__endswith="?", number_of_comments__range=(50, 101)
     ).all()
-    return posts
+    return list(posts)
 
 
-def exclude_like_comment_5():
+def exclude_like_comment_5() -> list[Post]:
     posts = (
         Post.objects.exclude(number_of_comments__gte=100)
         .exclude(number_of_likes__lte=50)
         .all()
     )
-    return posts
+    return list(posts)
 
 
 # def fetch_comments_or_likes_6():
@@ -46,19 +48,21 @@ def exclude_like_comment_5():
 #     return valid_posts
 
 
-def fetch_comments_or_likes_with_q_6():
+def fetch_comments_or_likes_with_q_6() -> list[Post]:
     posts = Post.objects.filter(
         Q(number_of_likes__gt=50) | Q(number_of_comments__lt=100)
     ).all()
-    return posts
+    return list(posts)
 
 
-def find_question_in_title_and_description_7():
+def find_question_in_title_and_description_7() -> Post | Any:
     post = Post.objects.filter(title__contains="?", description__contains="?").first()
     return post
 
 
-def find_favorites_8(new: bool = False, hot: bool = False, question: bool = False):
+def find_favorites_8(
+    new: bool = False, hot: bool = False, question: bool = False
+) -> list[Post]:
     qs = Post.objects.all()
     if new:
         qs = qs.filter(pub_date__gte=date.today() - timedelta(days=30))
@@ -67,4 +71,4 @@ def find_favorites_8(new: bool = False, hot: bool = False, question: bool = Fals
     if question:
         qs = qs.filter(title__contains="?", description__contains="?")
 
-    return qs
+    return list(qs)
